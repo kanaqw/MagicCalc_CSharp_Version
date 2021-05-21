b8950c5322bec7caf66be6da3cf710f386f04999
kanaqw <81157913+kanaqw@users.noreply.github.com>
2021-05-21 09:17:22 +0700

    Create Magic.cs

---
+++ Magic.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+     static void Main(string []args)
+        {
+            double a = double.PositiveInfinity/double.PositiveInfinity;
+            int x = (int)(7 + 3.0 / 4.0 * 2);
+            Console.WriteLine(a);
+            Console.WriteLine(x);
+            Console.WriteLine((1 + 1) / 2 * 3);
+            Console.ReadKey();
+        }       
+    }
