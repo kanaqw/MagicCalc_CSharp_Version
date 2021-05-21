a6c7dded49a54680a2b693fe73568300b85ebdd3
kanaqw <81157913+kanaqw@users.noreply.github.com>
2021-05-21 10:19:35 +0700

    Update Magic.cs

--- Magic.cs
+++ Magic.cs
@@ -6,16 +6,44 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+    public struct Calc
+    {
+        public void Calculate(int f, int s, string op)
+        {
+            switch (op)
+            {
+                case "sum":
+                    Console.WriteLine($"The result is {f+s}");
+                    break;
+                case "div":
+                    Console.WriteLine($"The result is {f / s}");
+                    break;
+                case "mul":
+                    Console.WriteLine($"The result is {f * s}");
+                    break;
+                case "sub":
+                    Console.WriteLine($"The result is {f - s}");
+                    break;
+
+            }
+            Console.ReadLine();
+        }
+    }
+
     class Program
     {
 
      static void Main(string []args)
         {
-            double a = double.PositiveInfinity/double.PositiveInfinity;
-            int x = (int)(7 + 3.0 / 4.0 * 2);
-            Console.WriteLine(a);
-            Console.WriteLine(x);
-            Console.WriteLine((1 + 1) / 2 * 3);
-            Console.ReadKey();
+            Console.WriteLine("Choose the operation: \n(hint: sum,sub,div or mul)");
+            string op = Console.ReadLine();
+            Console.WriteLine("First variable: ");
+            var first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Second variable: ");
+            var second = Convert.ToInt32(Console.ReadLine());
+            Calc calculator = new Calc();
+            calculator.Calculate(first, second, op);
+
         }       
     }
+}
